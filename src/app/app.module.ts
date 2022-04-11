b145cb32c641df03474ba5fc9569dc28fbf35123
FelipetLago <ftakaolago@gmail.com>
2022-04-10 23:24:44 -0300

    Criacao da estrutura base de Avaliacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { CategoriaDialogComponent } from './shared/categoria-dialog/categoria-dialog.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { AvaliacaoComponent } from './views/avaliacao/avaliacao.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     FooterComponent,
     BrowserGameDialogComponent,
     CategoriaComponent,
-    CategoriaDialogComponent
+    CategoriaDialogComponent,
+    AvaliacaoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/avaliacao/avaliacao.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AvaliacaoComponent } from './avaliacao.component';
+
+describe('AvaliacaoComponent', () => {
+  let component: AvaliacaoComponent;
+  let fixture: ComponentFixture<AvaliacaoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AvaliacaoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AvaliacaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
